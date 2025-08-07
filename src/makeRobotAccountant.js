09e61b04c66b934043c0f8ea8561d8a1ca2ae84f
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2025-08-07 12:11:27 +0200

    Solution

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,18 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let callCount = 0;
+
+  return function(a) {
+    return function(b) {
+      callCount++;
+      if callCount <= 3 {
+        return a + b
+      } else {
+        return callCount % 2 === 0 ? "Bzzz... Error!" : a + b;
+      }
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
